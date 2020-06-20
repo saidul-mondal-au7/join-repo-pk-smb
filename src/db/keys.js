8738cca356d83edee7f93beff9dda1516b7c12a9
Saidul Mondal <mondalsaidul3232@gmail.com>
2020-06-20 17:03:28 +0530

    Delete keys.js

--- src/db/keys.js
+++
@@ -1,5 +0,0 @@
-dbPassword = `mongodb+srv://${process.env.NAME}`+ encodeURIComponent(process.env.PASSWORD) + `${process.env.CLUSTER}.mongodb.net/test?retryWrites=true`;
-module.exports = {
-    mongoURI: dbPassword
-};
-// 
